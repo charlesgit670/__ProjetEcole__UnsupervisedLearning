c4ac346696c431db0cb624d14e1606e06ba8a2c1
Your Name <you@example.com>
2024-06-27 17:12:28 +0200

    ça avance

--- src/SOM/SOM.py
+++ src/SOM/SOM.py
@@ -67,15 +67,81 @@ class SOM:
                 plt.axis('off')
         plt.show()
 
-    # def plot(self, data, labels):
-    #     mapped = self.map_vects(data)
-    #     plt.figure(figsize=(10, 10))
-    #     for i, m in enumerate(mapped):
-    #         plt.text(m[1], m[0], str(labels[i]), color=plt.cm.rainbow(labels[i] / 10),
-    #                  fontdict={'weight': 'bold', 'size': 9})
-    #     plt.xlim([0, self.m])
-    #     plt.ylim([0, self.n])
-    #     plt.show()
+    def plot_label_histograms_per_cluster(self, data, labels):
+        mapped = self.map_vects(data)
+        label_counts = np.zeros((self.m, self.n, 10))
+
+        for i, m in enumerate(mapped):
+            label_counts[m[0], m[1], labels[i]] += 1
+
+        plt.figure(figsize=(25, 25))
+        for i in range(self.m):
+            for j in range(self.n):
+                plt.subplot(self.m, self.n, i * self.n + j + 1)
+                plt.bar(range(10), label_counts[i, j])
+                plt.xticks(range(10))
+                plt.yticks([])
+                plt.title(f'({i},{j})')
+        plt.show()
+
+    def plot_reconstructed_images(self, data, labels, gamma):
+        fig, axes = plt.subplots(3, 10, figsize=(20, 6))
+
+        # Afficher un exemple par label et son neurone BMU
+        unique_labels = np.unique(labels)
+        weights_flatten = self.weights.reshape(self.n * self.m, data.shape[1])
+        for label in unique_labels:
+            indices = np.where(labels == label)[0]
+
+            example_index = indices[0]
+            example_image = data[example_index]
+            bmu = self._find_bmu(example_image)
+            bmu_image = self.weights[bmu].reshape(28, 28)
+
+            axes[0, label].imshow(example_image.reshape(28, 28), cmap='gray')
+            axes[0, label].axis('off')
+            axes[0, label].set_title(f'Original {label}')
+
+            axes[1, label].imshow(bmu_image, cmap='gray')
+            axes[1, label].axis('off')
+            axes[1, label].set_title(f'BMU {label}')
+
+            # Calculer l'image combinée
+            distances = np.linalg.norm(example_image - weights_flatten, axis=1)
+            ponderations = np.exp(-distances ** 2 / gamma)
+            ponderations /= ponderations.sum(keepdims=True)
+            combined_image = np.dot(weights_flatten.T, ponderations)
+
+            axes[2, label].imshow(combined_image.reshape(28, 28), cmap='gray')
+            axes[2, label].axis('off')
+            axes[2, label].set_title('Combined Image')
+
+        plt.tight_layout()
+        plt.show()
+
+    def interpolate(self, x, y):
+        # Calculer les coordonnées des quatre neurones les plus proches
+        x1, y1 = int(np.floor(x)), int(np.floor(y))
+        x2, y2 = min(x1 + 1, self.m - 1), min(y1 + 1, self.n - 1)
+
+        # Calculer les distances fractionnelles
+        dx, dy = x - x1, y - y1
+
+        # Obtenir les poids des neurones
+        w11 = self.weights[x1, y1]
+        w12 = self.weights[x1, y2]
+        w21 = self.weights[x2, y1]
+        w22 = self.weights[x2, y2]
+
+        # Interpolation bilinéaire
+        interpolated_weight = (
+                (1 - dx) * (1 - dy) * w11 +
+                (1 - dx) * dy * w12 +
+                dx * (1 - dy) * w21 +
+                dx * dy * w22
+        )
+
+        return interpolated_weight.reshape(28, 28)
 
 if __name__ == "__main__":
     # Charger les données MNIST
@@ -86,10 +152,17 @@ if __name__ == "__main__":
     X_test = X_test.reshape((X_test.shape[0], -1)).astype(np.float32) / 255.0
 
     # Initialiser et entraîner la SOM
-    som = SOM(10, 10, learning_rate=0.1, gamma=0.5, n_iterations=10000)
+    som = SOM(10, 10, learning_rate=0.1, gamma=1.5, n_iterations=10000)
     som.train(X_train)
 
-    som.plot_weights()
-    # Visualiser les résultats
-    # som.plot(X_train, y_train)
-    # som.plot(X_test, y_test)
+    # Affiche la représentation de la map (espace latent)
+    # som.plot_weights()
+
+    # Affiche l'espace latent sous forme d'histogramme de label par cluster
+    # som.plot_label_histograms_per_cluster(X_test, y_test)
+
+    # Compression/décompression
+    # som.plot_reconstructed_images(X_test, y_test, 20)
+
+    # Génération à partir de l'interpolation entre 3 clusters
+    som.interpolate(2.5, 5.6)
