a9b378ce8e26ec1bf6542d6569a471d6ca2aca0f
charles <charles.logeais@yahoo.fr>
2024-06-28 02:05:53 +0200

    script resize image script load data food

---
+++ src/load_image.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+
+
+def load_data_food():
+    # Chemin vers le répertoire contenant les dossiers d'images par classe
+    dossier_images = '../../data/food-101-resize-selected/'
+
+    # Listes pour stocker les images et les labels
+    images = []
+    labels = []
+    label_to_classname = {}
+
+    # Parcourir tous les dossiers dans le répertoire
+    for label, class_folder in enumerate(sorted(os.listdir(dossier_images))):
+        class_folder_path = os.path.join(dossier_images, class_folder)
+
+        if os.path.isdir(class_folder_path):
+            label_to_classname[label] = class_folder
+            # Parcourir tous les fichiers dans le dossier de la classe
+            for filename in os.listdir(class_folder_path):
+                file_path = os.path.join(class_folder_path, filename)
+                if os.path.isfile(file_path):
+                    try:
+                        # Charger l'image avec PIL
+                        with Image.open(file_path) as img:
+                            # Redimensionner l'image si nécessaire
+                            img = img.resize((32, 32))
+
+                            # Convertir l'image en tableau numpy
+                            img_array = np.array(img)
+
+                            # Ajouter l'image et son label à la liste
+                            images.append(img_array)
+                            labels.append(label)
+
+                    except Exception as e:
+                        print(f'Erreur lors du traitement de {filename} : {e}')
+
+    images = np.array(images)
+    labels = np.array(labels)
+
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
+
+    return X_train, X_test, y_train, y_test, label_to_classname
+
+# Convertir les listes en tableaux numpy
+# X_train, X_test, y_train, y_test, label_to_classname = load_data_food()
+
---
+++ src/resize_image.py
@@ -0,0 +1,37 @@
+from PIL import Image
+import os
+from tqdm import tqdm
+
+# Chemin vers le répertoire contenant les dossiers d'images
+dossier_images = '../data/food-101/food-101/images/'
+# Chemin vers le nouveau répertoire où sauvegarder les images redimensionnées
+nouveau_dossier_images = '../data/food-101-resize/'
+
+# Parcourir tous les dossiers dans le répertoire
+for dossier in tqdm(os.listdir(dossier_images)):
+    dossier_path = os.path.join(dossier_images, dossier)
+    if os.path.isdir(dossier_path):
+        # Parcourir tous les fichiers dans le dossier
+        for filename in os.listdir(dossier_path):
+            file_path = os.path.join(dossier_path, filename)
+            if os.path.isfile(file_path):
+                try:
+                    # Ouvrir l'image avec PIL
+                    with Image.open(file_path) as img:
+                        # Redimensionner l'image en (32, 32)
+                        img_resized = img.resize((32, 32))
+
+                        # Convertir l'image en mode RGB (si elle ne l'est pas déjà)
+                        img_rgb = img_resized.convert('RGB')
+
+                        # Construire le chemin où sauvegarder l'image redimensionnée
+                        relative_path = os.path.relpath(file_path, dossier_images)
+                        new_save_path = os.path.join(nouveau_dossier_images, relative_path)
+                        os.makedirs(os.path.dirname(new_save_path), exist_ok=True)
+
+                        # Sauvegarder l'image redimensionnée
+                        img_rgb.save(new_save_path)
+                        # print(f'{filename} redimensionné et enregistré avec succès.')
+                except Exception as e:
+                    print(f'Erreur lors du traitement de {filename} : {e}')
+
